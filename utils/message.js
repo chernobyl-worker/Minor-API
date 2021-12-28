959e9e37916440827aaad55a2b70c3a1a602fb6d
chernobyl-worker <tushargupta1999@gmail.com>
2021-12-28 22:15:19 +0530

    Added message

--- utils/message.js
+++ utils/message.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 
 
 function sendText(details) {
-    const token = "5099022913:AAFFvQUhwj5KabVl_Uwxhfsh9i-29u4u6xw";
+    const token = env.API_KEY;
     const bot = new TelegramBot(token);
 
     bot.sendMessage(details.chatid, `Alcohol was detected by the device with the ID: ${details.id}. Please take appropriate action.`);
