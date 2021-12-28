1729f7b88a9ec2e22d4e95d34d37665837530443
chernobyl-worker <53166158+chernobyl-worker@users.noreply.github.com>
2021-12-28 22:18:18 +0530

    Update message.js

--- utils/message.js
+++ utils/message.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 
 
 function sendText(details) {
-    const token = env.API_KEY;
+    const token = process.env.API_KEY;
     const bot = new TelegramBot(token);
 
     bot.sendMessage(details.chatid, `Alcohol was detected by the device with the ID: ${details.id}. Please take appropriate action.`);
